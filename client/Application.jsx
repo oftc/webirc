aeae03459048a959ee8f227e0211b89bc7c62d2c
Stuart Walsh <stu@ipng.org.uk>
2016-09-09 17:13:10 +0100

    Automatically set nickname if it is set in the query string

--- client/Application.jsx
+++ client/Application.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import Login from './Login.jsx';
 import Irc from './Irc.jsx';
+import $ from 'jquery';
 
 class Application extends React.Component {
     constructor() {
@@ -12,18 +13,42 @@ class Application extends React.Component {
         this.state = { appState: 'login', stateArg: '' };
     }
 
+    componentWillMount() {
+        var nickname = $.getUrlVar('nickname');
+        if(nickname) {
+            this.setState({ appState: 'irc', stateArg: nickname });
+        }
+    }
+
     onStateChanged(newState, stateArg) {
         this.setState({ appState: newState, stateArg: stateArg });
     }
 
     render() {
         var component = this.state.appState === 'login' ? <Login onStateChanged={ this.onStateChanged } /> : <Irc nickname={ this.state.stateArg } />;
-        
+
         return (<div>
             { component }
         </div>);
     }
 }
+
+$.extend({
+    getUrlVars: function () {
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for (var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+
+        return vars;
+    },
+    getUrlVar: function (name) {
+        return $.getUrlVars()[name];
+    }
+});
 
 if (!window.__karma__) {
     render(<Application />, document.getElementById('component'));
